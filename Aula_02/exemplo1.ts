3110d66e52993409d19514080b367ceb6a55560e
GustavoUrban <gustavo.urban@fatec.sp.gov.br>
2023-08-14 22:20:03 -0300

    Aula 02

---
+++ Aula_02/exemplo1.ts
@@ -0,0 +1,48 @@
+namespace exemplo {
+    let valor: number = 1; 
+    let cont: number = 0;
+    do {
+            console.log (`Contador: ${valor}`)
+            cont++
+            cont = 10 ? valor = 0 : valor = 1
+        }
+        while (valor >= 1);
+
+
+    while (valor >= 0) {
+        console.log("Entrou no while");
+        valor ++
+        if(valor >= 20){
+            break;
+        }
+    }
+
+    for (let i = 1; i <= 10; i++){
+        console.log (i);
+    }
+
+    const nome = "carro"
+        for (const letras of nome){ 
+            console.log (letras) 
+        }
+
+    let vetor =[1,2,3,4,5]
+        for(const index in vetor){
+            console.log(index);
+        }
+
+        for(const valor of vetor){
+            console.log(valor);
+        } 
+
+    const carro = {
+        marca: "Gol",
+        ano: 1998,
+        cor:"Roxo"
+    }
+    for(let props in carro) {
+        console.log(`${props}: ${carro[props]}`);
+        
+    }
+
+}
